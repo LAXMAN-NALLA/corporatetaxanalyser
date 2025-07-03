ad8e72383ea8cabf0c99dedf5f2a2d0088747007
NALLA LAXMAN <laxmannalla337@gmail.com>
2025-07-03 17:08:25 +0530

    feat: Increase gunicorn timeout to 120s

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -3,17 +3,20 @@
 import os
 
 # Server socket
-# Gunicorn will listen on this host and port. Render provides the PORT env var.
 host = os.environ.get("HOST", "0.0.0.0")
 port = os.environ.get("PORT", "8000")
 bind = f"{host}:{port}"
 
 # Worker processes
-# The number of worker processes to spawn. A good starting point is (2 x $num_cores) + 1
 workers = int(os.environ.get("WEB_CONCURRENCY", 3))
 worker_class = "uvicorn.workers.UvicornWorker"
 
+# Timeout setting (THIS IS THE FIX)
+# The number of seconds to wait for a worker to send a response.
+# Default is 30s. We are increasing it to 2 minutes.
+timeout = 120
+
 # Logging
 loglevel = os.environ.get("LOG_LEVEL", "info")
-accesslog = "-"  # Log to stdout
-errorlog = "-"   # Log to stderr
+accesslog = "-"
+errorlog = "-"
